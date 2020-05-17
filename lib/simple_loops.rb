5b1044fd418977ab379ee0b99485ee11217c7ca8
Zana H <zhertica@gmail.com>
2020-05-17 03:05:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,8 +24,10 @@ end
 
 def return_string_array(array)
   count = 0
+  new_array = []
   while count < array.length do
-    array = array[count].to_s
+    new_array.push(array[count].to_s)
     count+=1
   end  
+  new_array
 end  
